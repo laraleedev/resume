783e71bbccf36a17b8ebf951218e66b3123f139e
Lara Lee <laralee@laras-mbp.lan>
2023-06-05 15:28:41 -0700

    Move exp to json and add script to generate versions
    Allows single set of exp data to maintain

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 Based on https://github.com/posquit0/Awesome-CV
 
 `xelatex resume.tex` to build
+
+## Usage
+Import the files into Overleaf and set the compiler to XeLaTeX
+
+## Experience generator
+Handles creation of abbreviated and full versions of experience to adjust resume length down as needed. This way only one set of experience data needs to be maintained
+
+Built using node v18.16.0
+
+Generates two files: `_FULL-experience.tex` and `experience.tex`, based on the data in `experience.json`. Latter output is abbreviated based on setting `"exclude": true` in the json.
+
+Run: `node ./generateExperience.mjs`. Assumes resume folder is created. Import output into overleaf as usual
---
+++ generateExperience.mjs
@@ -0,0 +1,89 @@
+import * as fs from "node:fs";
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+
+const jsonExp = JSON.parse(fs.readFileSync("./experience.json"));
+
+/********************************************************
+ * Helpers
+ ********************************************************/
+function commentBlock(logger, name) {
+    commentLine(logger);
+    logger.write(`%    ${name}\n`);
+    commentLine(logger);
+}
+
+function commentLine(logger) {
+    logger.write(`%-------------------------------------------------------------------------------\n`);
+}
+
+function commonHeader(logger) {
+    commentBlock(logger, "SECTION TITLE")
+    latexCommand(logger, "cvsection", "Work Experience");
+    commentBlock(logger, "CONTENT");
+    latexCommand(logger, "begin", "cventries");
+}
+
+function commonFooter(logger) {
+    commentLine(logger);
+    latexCommand(logger, "end", "cventries");
+}
+
+// Generates a \latex{thing}
+function latexCommand(logger, name, content) {
+    logger.write(`\\${name}` + (content ? `{${content}}` : "") + `\n`);
+}
+
+function writeCvEntry(logger, cventry, abbreviated) {
+    commentLine(logger);
+    latexCommand(logger, "cventry");
+    logger.write(`{${cventry.job}} % Job title\n`);
+    logger.write(`{${cventry.org}} % Organization\n`);
+    logger.write(`{${cventry.location}} % Location\n`);
+    logger.write(`{${cventry.date}} % Date(s)\n`);
+
+
+    if (abbreviated && cventry.exclude) {
+        logger.write(`{}\n`);
+    } else {
+        logger.write(`{\n`);
+        latexCommand(logger, "begin", "cvitems");
+        logger.write(`\\item[] {${cventry.cvitems.header}}\n`);
+        logger.write(`\\item[]\n`);
+
+        for (let index = 0; index < cventry.cvitems.items.length; index++) {
+            logger.write(`\\item {${cventry.cvitems.items[index]}}\n`);
+        }
+
+        latexCommand(logger, "end", "cvitems");
+        logger.write(`}\n\n`);
+    }
+}
+
+/********************************************************
+ * Full Exp Output
+ ********************************************************/
+var full = fs.createWriteStream("resume/_FULL-experience.tex", {});
+
+commonHeader(full);
+
+for (let index = 0; index < jsonExp.length; index++) {
+    writeCvEntry(full, jsonExp[index]);
+}
+
+commonFooter(full);
+full.end();
+
+/********************************************************
+ * Abbreviated Exp Output
+ ********************************************************/
+var abbreviated = fs.createWriteStream("resume/experience.tex", {});
+
+commonHeader(abbreviated);
+
+for (let index = 0; index < jsonExp.length; index++) {
+    writeCvEntry(abbreviated, jsonExp[index], true);
+}
+
+commonFooter(abbreviated);
+abbreviated.end();
